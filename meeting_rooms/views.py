dba2602fd486b3187b6be320ff69df591b8a997f
Petrauskas <petra.gytis@gmail.com>
2022-04-11 19:59:58 +0300

    Remove unused imports;

--- meeting_rooms/views.py
+++ meeting_rooms/views.py
@@ -1,5 +1,4 @@
-from rest_framework.response import Response
-from rest_framework import status, viewsets
+from rest_framework import viewsets
 
 from .serializers import MeetingRoomSerializer
 from .models import MeetingRoom
