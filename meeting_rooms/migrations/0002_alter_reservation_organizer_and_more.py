7c6bf9f1cd1b42305b1250db190202c7196c1f88
Petrauskas <petra.gytis@gmail.com>
2022-06-07 18:45:09 +0300

    Flake8 fixes

--- meeting_rooms/migrations/0002_alter_reservation_organizer_and_more.py
+++ meeting_rooms/migrations/0002_alter_reservation_organizer_and_more.py
@@ -16,7 +16,8 @@ class Migration(migrations.Migration):
         migrations.AlterField(
             model_name='reservation',
             name='organizer',
-            field=models.ForeignKey(default=1, on_delete=django.db.models.deletion.CASCADE, related_name='organizer', to=settings.AUTH_USER_MODEL),
+            field=models.ForeignKey(default=1, on_delete=django.db.models.deletion.CASCADE,
+                                    related_name='organizer', to=settings.AUTH_USER_MODEL),
             preserve_default=False,
         ),
         migrations.AlterField(
